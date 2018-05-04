28f4ef20f90368d1179323ba8f47474fdf0d5185
翁才晉 <cba46532@gmail.com>
2018-05-04 17:52:37 +0800

    add hello world

---
+++ test4/pages/index.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  h1.h1(@click="show") HELLO WORLD
+</template>
+
+<script lang="babel">
+  import AppLogo from '~/components/AppLogo.vue'
+
+  export default {
+    components: {
+      AppLogo
+    },
+    created(){
+
+      console.clear();
+    },
+    methods: {
+      show() {
+        alert('name');
+      }
+      
+    }
+  }
+</script>
+
+<style lang="sass">
+  .h1 
+    color: red
+</style>
